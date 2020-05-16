51163f58a93f30fd636dfbc1c8ce713708d5524b
niklek <nikitaleksikov@gmail.com>
2020-05-16 16:30:29 +0200

    Compare detecting a substring in a string

---
+++ substring_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+)
+
+const (
+	str    = "something"
+	substr = "some"
+)
+
+var (
+	re = regexp.MustCompile(substr)
+)
+
+func BenchmarkStringsContains(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = strings.Contains(str, substr)
+	}
+}
+
+func BenchmarkStringsIndex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = strings.Index(str, substr)
+	}
+}
+
+func BenchmarkStringsSplit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = strings.Split(str, substr)
+	}
+}
+
+func BenchmarkStringsCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = strings.Count(str, substr)
+	}
+}
+
+func BenchmarkStringsRegExp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = regexp.MatchString(substr, str)
+	}
+}
+
+func BenchmarkStringsRegExpCompiled(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = re.MatchString(str)
+	}
+}
